0cdb95a67c3c232d089a2a915487e718d6026c9b
vsapountzis <evangelos@sapountzis.gr>
2021-02-16 12:02:45 +0200

    Create wpautoinstall.sh

---
+++ wpautoinstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+wp theme install generatepress
+wp theme install https://generatepress.com/api/themes/generatepress_child.zip --activate
+wp theme delete twentynineteen
+wp theme delete twentytwenty
+wp plugin install updraftplus --activate
+wp plugin install wordfence --activate
+wp plugin install classic-editor --activate
+wp plugin install duplicate-post --activate
