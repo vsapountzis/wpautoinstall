8158fa2c1fbc16809b839a88ac8ac30e461140d8
vsapountzis <evangelos@sapountzis.gr>
2021-02-16 12:40:46 +0200

    added elementor plugin to the install script

--- wpautoinstall.sh
+++ wpautoinstall.sh
@@ -3,6 +3,7 @@ wp theme install generatepress
 wp theme install https://generatepress.com/api/themes/generatepress_child.zip --activate
 wp theme delete twentynineteen
 wp theme delete twentytwenty
+wp plugin install elementor --activate
 wp plugin install updraftplus --activate
 wp plugin install wordfence --activate
 wp plugin install classic-editor --activate
